2224d609d85d16455e25dfcbb62952f14aa32a97
陈晓东 <xiaodong.chen01@hand-china.com>
2020-08-24 18:32:00 +0800

    新增 nacos

--- cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
@@ -6,9 +6,14 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * @author xiaoChen
+ *
+ */
 @RestController
 @Slf4j
-@RefreshScope
+@RefreshScope  /**支持nacos动态刷新功能*/
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
